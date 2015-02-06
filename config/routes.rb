31693f30f0179fb12865e41c185491fdf9410e61
Jaime <jpanaia@gmail.com>
2015-02-06 12:20:10 -0500

    added some pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'main/index'
+  root 'main#index'
 
-  get 'main/thanks'
+  get 'thanks' => 'main#thanks'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
