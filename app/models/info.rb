d47a80f98cf8ca5bb5f5e9c4f284dd83e798c4bc
Jaime <jpanaia@gmail.com>
2015-02-07 22:46:30 -0500

    email validation

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,3 +1,3 @@
 class Info < ActiveRecord::Base
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
